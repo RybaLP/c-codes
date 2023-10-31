933953af815d2a9b9802e3ee0da72fad386663da
RybaLP <148885360+RybaLP@users.noreply.github.com>
2023-10-31 08:46:49 +0100

    Add files via upload

---
+++ 7zadanko.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ///podaj ile chcesz dodac liczb do listy
+        ///podaje liczbe
+        ///podaj liczby do listy:
+        ///petla sie konczy gdy podasz wystarczajaco duzo \
+
+        Console.WriteLine("Podaj ile chcesz miec liczb w tablicy ");
+        int n = int.Parse(Console.ReadLine());
+        int[] tablica = new int [n];
+        for(int i=0; i<n; i++)
+        {
+            Console.WriteLine($"Liczba: {i + 1}");
+            tablica[i] = int.Parse(Console.ReadLine());
+        }
+        foreach (int k in tablica)
+        {
+            Console.Write(k + " \n");
+        }
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - 1 - i; j++)
+            {
+                if (tablica[j] > tablica[j + 1])
+                {
+                    int temp = tablica[j];
+                    tablica[j] = tablica[j + 1];
+                    tablica[j + 1] = temp;
+                }
+            }
+        }
+        Console.WriteLine("Posortowana tablica bubble sortem:");
+        foreach (int liczba in tablica)
+        {
+            Console.Write(liczba + " ");
+        }
+    }
+}
---
+++ zadanie6.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+
+            Console.WriteLine("Podaj liczbe ");
+            int liczba = int.Parse(Console.ReadLine());
+            if (liczba < 0)
+            {
+                Console.Clear();
+                Console.WriteLine("Koniec programu!");
+                break;
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
